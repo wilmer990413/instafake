254ea0b0052d31d1d0059f3187ab48b8285b7654
Jesus-Garcia-Montalvo <jesus.antonio.garcia9871@gmail.com>
2023-11-05 23:12:20 -0500

    Vista Rama Feed

--- src/pages/postDetail/PostDetail.jsx
+++ src/pages/postDetail/PostDetail.jsx
@@ -1,21 +1,7 @@
 import React from "react";
-import Header from "../../components/header/Header";
-import Logo from "../../components/Logo/Logo";
-import Historia from "../../components/Historias/Historia";
-import Publicacion from "../../components/Publicacion/Publicacion";
-import Footer from "../../components/footer/Footer";
-import "./postDetail.css";
 
 const PostDetail = () => {
-  return (
-    <div className="contenedor">
-      <Header />
-      <Logo />
-      <Historia />
-      <Publicacion />
-      <Footer />
-    </div>
-  );
+  return <div>PostDetail</div>;
 };
 
 export default PostDetail;
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Menu from "../components/Menu";
-import Feed from "../pages/Feed";
+
 import Profile from "../pages/profile/Profile";
 
 import Login from "../pages/login/Login";
 import Register from "../pages/register/Register";
 import PostDetail from "../pages/postDetail/PostDetail";
+import Feed from "../pages/feed/Feed";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
         <Route path="login" element={<Login />}></Route>
         <Route path="register" element={<Register />}></Route>
         <Route path="PostDetail" element={<PostDetail />}></Route>
+        <Route path="Feed" element={<Feed />}></Route>
       </Routes>
     </BrowserRouter>
   );
