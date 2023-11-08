0cf2efa71c1de73deabd2978f571ea9ccf386f86
Wilmer Rodolfo Castro Ladino <wilmer.castro@unillanos.edu.co>
2023-11-07 20:10:17 -0500

    validando

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { useAuth } from "../data/userContext";
 
 const Feed = () => {
+  const { autenticado } = useAuth();
   return (
     <div>
       Feed
+      {
+        autenticado === true? "Entro" : "No ENTRO"
+      }
       <button>1</button>
     </div>
   );
